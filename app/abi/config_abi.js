be3c22cba507f370cb97ddef48f583e63296f0df
lukydwisaputra <lukydwisaputra@gmail.com>
2023-02-07 11:31:32 +0700

    initial commit

---
+++ app/abi/config_abi.js
@@ -0,0 +1,19 @@
+const ERC20 = require('./ERC20.json');
+const FACTORY = require('./FACTORY.json');
+const MASTER = require('./MASTER.json');
+const MULTICALL_STATIC = require('./MULTICALL_STATIC.json');
+const PAIR = require('./PAIR.json');
+const ROUTER = require('./ROUTER.json');
+const WETH = require('./WETH.json');
+
+const ABI = {
+	ERC20,
+	FACTORY,
+	MASTER,
+	MULTICALL_STATIC,
+	PAIR,
+	ROUTER,
+	WETH,
+};
+
+module.exports = { ABI };
